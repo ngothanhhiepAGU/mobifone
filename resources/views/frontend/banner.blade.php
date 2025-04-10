465d45fe0c2f5fca8afdf402f1cc1ef0ba1bd7f6
ngothanhhiepAGU <163821703+ngothanhhiepAGU@users.noreply.github.com>
2025-04-10 09:54:00 +0700

    Initial commit

--- resources/views/frontend/banner.blade.php
+++ resources/views/frontend/banner.blade.php
@@ -1,71 +1,40 @@
-<section id="billboard" class="position-relative d-flex align-items-center py-5 bg-light-gray"
-      style="background-image: url(images/banner-image-bg.jpg); background-size: cover; background-repeat: no-repeat; background-position: center; height: 800px;">
-      <div class="position-absolute end-0 pe-0 pe-xxl-5 me-0 me-xxl-5 swiper-next main-slider-button-next">
-        <svg class="chevron-forward-circle d-flex justify-content-center align-items-center p-2" width="80" height="80">
-          <use xlink:href="#alt-arrow-right-outline"></use>
-        </svg>
+<section id="billboard" class="position-relative py-5" style="
+  background: url('images/banner-image-bg.jpg') center center / cover no-repeat;
+  height: 800px;
+  display: flex;
+  align-items: center;
+">
+  <!-- Swiper navigation -->
+  <div class="position-absolute top-50 start-0 translate-middle-y ps-3 z-3 swiper-prev main-slider-button-prev">
+    <svg class="chevron-back-circle" width="60" height="60">
+      <use xlink:href="#alt-arrow-left-outline"></use>
+    </svg>
+  </div>
+  <div class="position-absolute top-50 end-0 translate-middle-y pe-3 z-3 swiper-next main-slider-button-next">
+    <svg class="chevron-forward-circle" width="60" height="60">
+      <use xlink:href="#alt-arrow-right-outline"></use>
+    </svg>
+  </div>
+
+  <!-- Swiper main -->
+  <div class="swiper main-swiper w-100">
+    <div class="swiper-wrapper">
+
+      <!-- Slide 1 -->
+      <div class="swiper-slide d-flex justify-content-center align-items-center">
+        <img src="images/banner-image2.png" class="img-fluid" style="max-height: 770px; width: auto; object-fit: cover;" alt="Banner Image 1">
       </div>
-      <div class="position-absolute start-0 ps-0 ps-xxl-5 ms-0 ms-xxl-5 swiper-prev main-slider-button-prev">
-        <svg class="chevron-back-circle d-flex justify-content-center align-items-center p-2" width="80" height="80">
-          <use xlink:href="#alt-arrow-left-outline"></use>
-        </svg>
+
+      <!-- Slide 2 -->
+      <div class="swiper-slide d-flex justify-content-center align-items-center">
+        <img src="images/banner-image1.png" class="img-fluid" style="max-height: 770px; width: auto; object-fit: cover;" alt="Banner Image 2">
       </div>
-      <div class="swiper main-swiper">
-        <div class="swiper-wrapper d-flex align-items-center">
-          <div class="swiper-slide">
-            <div class="container">
-              <div class="row d-flex flex-column-reverse flex-md-row align-items-center">
-                <div class="col-md-5 offset-md-1 mt-5 mt-md-0 text-center text-md-start">
-                  <div class="banner-content">
-                    <h2>The Fine Print Book Collection</h2>
-                    <p>Best Offer Save 30%. Grab it now!</p>
-                    <a href="index.html" class="btn mt-3">Shop Collection</a>
-                  </div>
-                </div>
-                <div class="col-md-6 text-center">
-                  <div class="image-holder">
-                    <img src="images/banner-image2.png" class="img-fluid" alt="banner">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="container">
-              <div class="row d-flex flex-column-reverse flex-md-row align-items-center">
-                <div class="col-md-5 offset-md-1 mt-5 mt-md-0 text-center text-md-start">
-                  <div class="banner-content">
-                    <h2>How Innovation works</h2>
-                    <p>Discount available. Grab it now!</p>
-                    <a href="index.html" class="btn mt-3">Shop Product</a>
-                  </div>
-                </div>
-                <div class="col-md-6 text-center">
-                  <div class="image-holder">
-                    <img src="images/banner-image1.png" class="img-fluid" alt="banner">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="container">
-              <div class="row d-flex flex-column-reverse flex-md-row align-items-center">
-                <div class="col-md-5 offset-md-1 mt-5 mt-md-0 text-center text-md-start">
-                  <div class="banner-content">
-                    <h2>Your Heart is the Sea</h2>
-                    <p>Limited stocks available. Grab it now!</p>
-                    <a href="index.html" class="btn mt-3">Shop Collection</a>
-                  </div>
-                </div>
-                <div class="col-md-6 text-center">
-                  <div class="image-holder">
-                    <img src="images/banner-image.png" class="img-fluid" alt="banner">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
+      <!-- Slide 3 -->
+      <div class="swiper-slide d-flex justify-content-center align-items-center">
+        <img src="images/banner-image.png" class="img-fluid" style="max-height: 770px; width: auto; object-fit: cover;" alt="Banner Image 3">
       </div>
-    </section>
+
+    </div>
+  </div>
+</section>
